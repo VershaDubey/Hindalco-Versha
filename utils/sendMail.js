760d069e227de5ee5e06bfd1ee28c0411a405179
Versha <versha@Vershas-MacBook-Pro.local>
2026-03-24 16:27:31 +0530

    fix ipv6 email issue

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,4 +1,8 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// 🔥 FORCE IPV4
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -8,7 +12,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-async function sendMail({ to, subject, html }) {
+async function sendMail({ to, subject, html, userName, rating }) {
   try {
     console.log("📧 Sending email →", to);
 
@@ -17,9 +21,21 @@ async function sendMail({ to, subject, html }) {
       to,
       subject,
       html,
+
+      text: `Hi ${userName},
+
+Thank you for your feedback.
+
+Rating: ${rating}/5
+
+Regards,
+Team Hindalco`,
+
+      replyTo: process.env.EMAIL_USER,
     });
 
     console.log("✅ Email sent");
+
   } catch (err) {
     console.error("❌ Mail error:", err.message);
     throw err;
